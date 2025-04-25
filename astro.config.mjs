58a0df276ea7d284f3c258fbd11da31127f3a96c
CarlosSolvas <c.solvas@gmail.com>
2025-04-25 19:02:30 +0200

    actualización

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
+export default {
+    site: 'https://CarlosSolvas.github.io/semana5',
+    build: {
+      out: './dist', // Esto asegura que todo se genere en la carpeta dist.
+    },
+    base: "/semana5/", // 👈 Esto es lo que hay que agregar para GitHub Pages
+  };
+  
